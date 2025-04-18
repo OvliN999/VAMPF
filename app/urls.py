c2c5c36646fc0f87de87c86c5b61fffe052e389e
OvliN999 <mukeshkumarjb0721@gmail.com>
2025-04-19 00:53:27 +0530

    Resolved merge conflicts

--- app/urls.py
+++ app/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path
 from . import views
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 from app.controller import authview, book, admins
 
 urlpatterns =[
+    path('admin/', admin.site.urls),
+    path('', include('app.urls')),
     path('', views.home, name="home"),
     path('category/', views.category, name="category"),
     path('category/<str:id>', views.categoryview, name="categoryview"),
@@ -31,3 +37,6 @@ urlpatterns =[
 
     
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
